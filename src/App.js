60552d06efd1cc7816033e6c33d52363384768b0
Chen <zchen100@MGC1KXMHGTJ1GP.warley.ford.com>
2019-03-22 16:30:09 +0000

    Button click

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import reactDOM from "react-dom";
 import './App.css';
 
 class App extends Component {
+
+    state = {
+      text: "Hi! My name is Zhen"
+    };
+
+  clicked() {
+    console.log()
+    this.setState({ text: this.refs.textBox.value })
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    <div>
+      { this.state.text}
+      <input ref="textBox" type="text" />
+      <button onClick = { () => { this.clicked(); }}> Submit </button>
+    </div> )
   }
 }
 
